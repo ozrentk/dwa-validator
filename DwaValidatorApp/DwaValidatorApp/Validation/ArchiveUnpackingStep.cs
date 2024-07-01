cdd6a99875ee1702cfc17bc1c277322ca61d3940
Ozren TK <ozren.krznaric@gmail.com>
2024-07-01 15:15:04 +0200

    Added (recursive) delete of the target folder

--- DwaValidatorApp/DwaValidatorApp/Validation/ArchiveUnpackingStep.cs
+++ DwaValidatorApp/DwaValidatorApp/Validation/ArchiveUnpackingStep.cs
@@ -10,6 +10,18 @@ namespace DwaValidatorApp.Validation
         private const string UnpackedArchivesFolderName = "UnpackedArchives";
         private const string DataFolderName = "Data";
 
+        public static void RecursiveDelete(DirectoryInfo baseDir)
+        {
+            if (!baseDir.Exists)
+                return;
+
+            foreach (var dir in baseDir.EnumerateDirectories())
+            {
+                RecursiveDelete(dir);
+            }
+            baseDir.Delete(true);
+        }
+
         public override async Task<ValidationResult> RunAsync(ValidationContext context) =>
             await Task.Run(() =>
             {
@@ -25,13 +37,16 @@ namespace DwaValidatorApp.Validation
                         context.Root,
                         DataFolderName);
 
-                // Unzip the archive
+                // Delete the target path and unzip the archive to the target path
                 string archivePath = context.SolutionArchivePath;
                 context.SolutionArchiveName = Path.GetFileNameWithoutExtension(archivePath);
 
                 try
                 {
                     context.ExtractedArchivePath = Path.Combine(context.UnpackedArchivesPath, context.SolutionArchiveName);
+
+                    DeleteTargetPath(context.ExtractedArchivePath, res);
+
                     ZipFile.ExtractToDirectory(
                         archivePath,
                         context.ExtractedArchivePath,
@@ -45,5 +60,35 @@ namespace DwaValidatorApp.Validation
 
                 return res;
             });
+
+        private static void DeleteTargetPath(string extractedArchivePath, ValidationResult res)
+        {
+            if (!Directory.Exists(extractedArchivePath))
+                return;
+
+            try
+            {
+                Directory.Delete(extractedArchivePath, true);
+
+                res.AddInfo($"Folder {extractedArchivePath} deleted");
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    res.AddInfo($"WARNING: Folder {extractedArchivePath} delete failed, trying recursive delete...");
+
+                    RecursiveDelete(
+                        baseDir: new DirectoryInfo(extractedArchivePath));
+                }
+                catch (Exception ex2)
+                {
+                    res.AddInfo($"WARNING: Folder {extractedArchivePath} recursive delete failed, delete manually");
+                    res.AddInfo(ex2.Message);
+                }
+
+                throw;
+            }
+        }
     }
 }
